8557d88ef5c98820c5f83381fc63124889e10d48
Siddhi Chauhan <infinitie.siddhi@gmail.com>
2025-05-19 14:53:49 +0530

    new jks added

--- android/app/src/main/kotlin/com/mntom/seller/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mntom.seller
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/mntom/sellermultivendor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mntom.sellermultivendor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
